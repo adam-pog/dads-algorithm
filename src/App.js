3b4207f3296dd95059377d6fe98b0df8d2d1920c
Adam Pogwizd <adampog77@gmail.com>
2022-12-16 19:55:37 -0700

    WIP

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+// var __html = require('./atom.html');
+// var template = { __html: __html};
+
+function App() {
+  return (
+    <div className="App">
+      <div className="atom-loader">
+        <div className="atom-inner atom-one"></div>
+        <div className="atom-inner atom-two"></div>
+        <div className="atom-inner atom-three"></div>
+        <div className="atom-inner atom-four"></div>
+        <div className="atom-inner atom-center-one"></div>
+        <div className="atom-inner atom-center-two"></div>
+        <div className="atom-inner atom-center-three"></div>
+        <div className="atom-inner atom-center-four"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
